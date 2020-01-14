1a6605dcecd81eed11b9939c8db8d0f8beb75f66
Mike Byce <mikebyce@gmail.com>
2020-01-14 00:07:17 -0600

    Removed TypeScript locally

--- src/ServiceRouter/index.ts
+++
@@ -1,7 +0,0 @@
-export const handler = async (event: any = {}): Promise<any> => {
-  // Log the event argument for debugging and for use in local development.
-  const response = JSON.stringify(event, undefined, 2);
-  console.log(response);
-
-  return response;
-}
